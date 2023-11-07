64fcac2c451ca24ad19ba6ae06e6601478b8776e
Yang Liu <YLiu148@slb.com>
2023-11-07 21:01:15 +0800

    test: add cteate article case

--- MiniBlogTest/ServiceTest/UserServiceTest.cs
+++ MiniBlogTest/ServiceTest/UserServiceTest.cs
@@ -76,5 +76,53 @@ namespace MiniBlogTest.ServiceTest
             Assert.Equal(email, users[0].Email);
             Assert.Equal(userName, users[0].Name);
         }
+
+
+        [Fact]
+        public async void Should_create_article_and_register_user_correct()
+        {
+            var mockArticleRepository = new Mock<IArticleRepository>();
+            var mockUserRepository = new Mock<IUserRepository>();
+            string userNameWhoWillAdd = "Tom";
+            string articleContent = "What a good day today!";
+            string articleTitle = "Good day";
+            Article article = new Article(userNameWhoWillAdd, articleTitle, articleContent);
+            mockArticleRepository.Setup(repository => repository.GetArticles()).Returns(Task.FromResult(new List<Article>
+            {
+                article
+            }));
+            User user = new User("Tom");
+            mockArticleRepository.Setup(repository => repository.CreateArticle(article)).Returns(Task.FromResult(article));
+
+            mockUserRepository.Setup(repository => repository.GetUsersAsync())
+                .Returns(Task.FromResult(new List<User> { user }));
+            mockUserRepository.Setup(repository => repository.CreateUserAsync(user))
+                .Returns(Task.FromResult(user));
+
+            var client = GetClient(null,null,  mockArticleRepository.Object, mockUserRepository.Object);
+
+            var httpContent = JsonConvert.SerializeObject(article);
+            StringContent content = new StringContent(httpContent, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var createArticleResponse = await client.PostAsync("/article", content);
+
+            // It fail, please help
+            Assert.Equal(HttpStatusCode.Created, createArticleResponse.StatusCode);
+
+            var articleResponse = await client.GetAsync("/article");
+            var body = await articleResponse.Content.ReadAsStringAsync();
+            var articles = JsonConvert.DeserializeObject<List<Article>>(body);
+            Assert.Equal(1, articles.Count);
+            Assert.Equal(articleTitle, articles[0].Title);
+            Assert.Equal(articleContent, articles[0].Content);
+            Assert.Equal(userNameWhoWillAdd, articles[0].UserName);
+
+            var userResponse = await client.GetAsync("/user");
+            var usersJson = await userResponse.Content.ReadAsStringAsync();
+            var users = JsonConvert.DeserializeObject<List<User>>(usersJson);
+
+            Assert.True(users.Count == 1);
+            Assert.Equal(userNameWhoWillAdd, users[0].Name);
+            Assert.Equal("anonymous@unknow.com", users[0].Email);
+        }
     }
 }
